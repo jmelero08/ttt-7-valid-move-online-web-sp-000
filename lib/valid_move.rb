4bca80d1da8dd3735055d1023d247e6280747155
Jose Melero Martinez <jmelero08@gmail.com>
2020-10-15 20:15:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,8 +4,6 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
- 
-
 def position_taken?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil 
     return false
